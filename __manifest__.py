b91476614effabce4081c146a0d48c1d26ab0109
Mohammad Zohair <mohammam.mz@gmail.com>
2024-03-09 13:52:02 +0300

    first

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Real Estate",
+
+    'sequence': -101,  
+    'application': True,
+    'category': 'Estate',    
+
+    'summary': "Real Estate Application",
+
+    'description': "Real Estate Application",
+
+    'author': "Mohammad Zohair",
+    'website': "",
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    
+    'data': [],
+    
+    'demo': [],
+}
