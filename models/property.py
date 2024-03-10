aa6db6f537dfd240d291c6456d20994d39d7a9f4
Mohammad Zohair <mohammam.mz@gmail.com>
2024-03-10 19:25:18 +0300

    added computed fields

--- models/property.py
+++ models/property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from datetime import timedelta
 
 class Property(models.Model):
@@ -11,14 +11,17 @@ class Property(models.Model):
     postcode = fields.Char()
     date_availability = fields.Date(default=fields.date.today() + timedelta(days=90))
     expected_price = fields.Float(required=True)
+    best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
     selling_price = fields.Float()
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(default=0)
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(default=0)
     garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', "East"), ('west', 'West')])
+
+    total_area = fields.Float(compute="_compute_total_area")
 
     salesperson = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True, default=lambda self: self.env.user)
     buyer = fields.Many2one('res.partner', copy=False, string="Buyer")
@@ -34,4 +37,21 @@ class Property(models.Model):
         ('sold', 'Sold'),
         ('cancelled', 'Cancelled')
     ], default='new')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+    
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            best_price = 0
+            for offer in record.offer_ids:
+                if offer.price > best_price:
+                    best_price = offer.price
+            
+            record.best_price = best_price
+        
 
